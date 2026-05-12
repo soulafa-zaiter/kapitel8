ab600a34b0bc33e9861509ddc966e3eefe25f960
Soulafa Zaiter <soulafa.zaiter@stud.hs-hannover.de>
2026-05-12 08:53:58 +0200

    Multiplikation wurde implementiert

--- Main.java
+++ Main.java
@@ -3,6 +3,16 @@ public class Main
 {
     public static void main(String args[])
     {
+        int summe = 0;
+        for(int i = 1; i <= 20; ++i)
+        {
+            summe = summe + 1;
+        }
         
+        int mult = 1;
+        for(int i = 1; i <= 10; ++i)
+        {
+            mult = mult * i;
+        }
     }
 }
