6ab997a35d8392290f93864206fba0a5a96a8691
soulafa-zaiter <soulafa.zaiter@stud.hs-hannover.de>
2026-05-19 09:02:10 +0200

    updateee

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import java.math.BigIntger;
 public class Main
 {
     public static void main(String args[])
@@ -12,10 +12,12 @@ public class Main
             summe += 1;
         }
         
-        double mult = 1;
+        BigInteger mult = new BigInteger("1");
+        
         for(int i = 1; i <= obereGrenze; ++i)
         {
-            mult *= i;
+            BigInteger tmp = new BigInteger(String.valueOf(i));
+            mult = mult.multiply(tmp);
         }
         
         System.out.println("Summe " + summe);
